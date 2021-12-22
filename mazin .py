e2170e039902c1a2323b03550a111c3bb41bad85
Ahmed770-hub <77691032+Ahmed770-hub@users.noreply.github.com>
2021-12-23 00:28:49 +0200

    Add files via upload

--- mazin .py
+++ mazin .py
@@ -15,14 +15,19 @@ import webbrowser
 import random
 
 
-user = input("Enter link : ")
+a = input("Enter link : ")
+u = input("Enter another link : ")
 view = int(input("Enter number of view : "))
 k = (view)/60
 print(f'''
 You will take {view} view in {k} Hours or {view} in minute''')
 lim = round(view)
 for i in range(0,lim):
-	webbrowser.open(user)
+	import random
+	list1=[u, a]
+	q=random.randint(0,1)
+	m=list1[q]
+	webbrowser.open(m)
 	print("Done you take 1 view")
 	time.sleep(5)
 
